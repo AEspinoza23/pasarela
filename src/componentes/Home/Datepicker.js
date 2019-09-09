5d47566660fb06c8f7ee28998ea9396675661041
Fidelmar Alexandra Gutierrez Vasquez <fgutierrez@surnet.io>
2019-09-09 16:55:19 -0400

    cambios date-picker

--- src/componentes/Home/Datepicker.js
+++ src/componentes/Home/Datepicker.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import ModernDatepicker from 'react-modern-datepicker';
-import { MDBModalBody, MDBModal, MDBModalHeader, MDBIcon, MDBView, MDBBtn, MDBModalFooter } from 'mdbreact';
+import { MDBModal, MDBIcon, MDBView, MDBRow} from 'mdbreact';
+import Calendar from 'react-calendar'
+
 
 export default class Datepicker extends React.Component {
   state = {
-    modal: false
-  }
-
-  constructor(props) {
-    super(props);
-    this.state = {
-        startDate: new Date(), // can be any of these ['dayjs()', '', null, new Date(2018,12,1)]
-    };
-    this.handleChange = this.handleChange.bind(this);
+    modal: false,
+    date: new Date()
   }
 
   toggle = () => {
@@ -20,12 +14,8 @@ export default class Datepicker extends React.Component {
       modal: !this.state.modal
     });
   }
- 
-  handleChange(date) {
-    this.setState({
-      startDate: date,
-    });
-  }
+
+  onChange = date => this.setState({ date })
  
   render() {
     return (
@@ -33,25 +23,14 @@ export default class Datepicker extends React.Component {
         <MDBView hover zoom>
           <MDBIcon className="px-2 hoverable" size="lg" far icon="calendar-alt" onClick={this.toggle} />
         </MDBView>
-        <MDBModal isOpen={this.state.modal} toggle={this.toggle}>
-          <MDBModalHeader className="colorConviasa" toggle={this.toggle}>Fecha ída</MDBModalHeader>
-          <MDBModalBody>
-            <ModernDatepicker
-              date={this.state.startDate}
-              format={'DD-MM-YYYY'}
-              showBorder
-              onChange={date => this.handleChange(date)}
-              placeholder={'Seleccione una fecha'}
-              primaryColor={'#EE7500'}
-              secondaryColor={'white'}
-              primaryTextColor={'#EE7500'}
-              secondaryTextColor={'white'}
-
+        <MDBModal isOpen={this.state.modal} toggle={this.toggle} size="sm">
+          <MDBRow center>
+            <Calendar
+            onChange={this.onChange}
+            value={this.state.date}
+            className="black-text"
             />
-          </MDBModalBody>
-          <MDBModalFooter>
-            <MDBBtn color="orange" onClick={this.toggle}>Aceptar</MDBBtn>
-        </MDBModalFooter>
+          </MDBRow>
         </MDBModal>
       </div>
     );
